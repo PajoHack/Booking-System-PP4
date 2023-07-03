58b1d83a78fad2efbad80ed10f1dc7f155cf41c0
Patrick Hackman <pajohack@gmail.com>
2023-07-03 22:50:57 +0000

    Add menu_list.html in the adminapp to allow admin to manage menu items

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,4 +6,8 @@ app_name = 'adminapp'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.admin_home, name='admin_home'),
+    path('menu/', views.menu_list, name='menu_list'),
+    path('menu/new/', views.menu_new, name='menu_new'),
+    path('menu/<int:pk>/edit/', views.menu_edit, name='menu_edit'),
+    path('menu/<int:pk>/delete/', views.menu_delete, name='menu_delete'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bookings.models import MenuItem
 
 
 def index(request):
@@ -9,3 +10,39 @@ def index(request):
 def admin_home(request):
     return render(request, 'adminapp/admin_home.html')
 
+def menu_list(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'adminapp/menu_list.html', {'menu_items': menu_items})
+
+def menu_new(request):
+    if request.method == "POST":
+        menu_item = MenuItem(
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+            price = request.POST.get('price'),
+            category = request.POST.get('category'),
+            image = request.POST.get('image'),
+        )
+        menu_item.save()
+        return redirect('adminapp:menu_edit', pk=menu_item.pk)
+    return render(request, 'adminapp/menu_form.html')
+
+def menu_edit(request, pk):
+    menu_item = get_object_or_404(MenuItem, pk=pk)
+    if request.method == "POST":
+        menu_item.name = request.POST.get('name')
+        menu_item.description = request.POST.get('description')
+        menu_item.price = request.POST.get('price')
+        menu_item.category = request.POST.get('category')
+        menu_item.image = request.POST.get('image')
+        menu_item.save()
+        return redirect('adminapp:menu_edit', pk=menu_item.pk)
+    return render(request, 'adminapp/menu_edit.html', {'menu_item': menu_item})
+
+
+def menu_delete(request, pk):
+    menu_item = get_object_or_404(MenuItem, pk=pk)
+    if request.method == "POST":
+        menu_item.delete()
+        return redirect('adminapp:menu_list')
+    return render(request, 'adminapp/menu_delete.html', {'menu_item': menu_item})
